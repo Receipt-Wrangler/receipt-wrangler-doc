7dbac41b3c05452d1645799ce62c023da39fdac4
Noah231515 <noah231515@gmail.com>
2025-01-21 20:57:27 +0000

    hide next

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -34,6 +34,7 @@ const config: Config = {
             "classic",
             {
                 docs: {
+                    includeCurrentVersion: false,
                     sidebarPath: "./sidebars.ts",
                 },
                 blog: {
